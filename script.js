f40053e004dcfcd63d4979a9fb5ca44a54f9ff19
ll560 <llavia6@gmail.com>
2022-05-07 01:15:31 -0400

    adding animation to guy

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const phaserConfig = {
       default: 'arcade',
       arcade: {
           debug: true,
-          gravity: { y: 200 }
+          gravity: { y: 300 }
       }
   },
     scene: {
@@ -29,7 +29,7 @@ const phaserConfig = {
 
 var cloudsWhite, cloudsWhiteSmall, platform;
 let diamonds;
-let icon;
+let player;
 
 function initScene() { }
 
@@ -39,15 +39,19 @@ function preloadScene() {
     this.load.image('platform','platform.png')
     this.load.image('bomb', 'mine.png')
     //this.load.image("clouds-white-small", "clouds-white-small.png");
-    this.load.image('star', 'diamond.png');
+    this.load.image('diamond', 'diamond.png');
     //this.load.image('diamonds', 'red-blood-cell.png')
-    this.load.spritesheet('dude','icon.png', {frameWidth:44, frameHeight: 38});
+    this.load.spritesheet('dude','dude.png', {frameWidth:32, frameHeight: 48});
+    
     
  }
 
  function createScene() {
     
     cloudsWhite = this.add.tileSprite(640, 200, 1280, 400, "clouds-white");
+
+    
+
 
     platform = this.physics.add.staticGroup()
     platform.enableBody = true;
@@ -56,66 +60,114 @@ function preloadScene() {
     platform.create(860, 500, 'platform') //ledge
     platform.create(50, 300, 'platform')
   
+    
+      
     //this is for our player
-    player= this.physics.add.sprite(icon, false)
-    icon.body.velocity.setTo(200,200);
-    icon.body.collideWorldBounds = true;
-    icon.body.bounce.set(1);
-    icon.setScale(0.9, 0.9);
+    
+    
+    player = this.physics.add.sprite(100, 450, 'dude')
+    //player.body.velocity.setTo(200,200);
+    player.body.collideWorldBounds = true;
+    player.body.bounce.set(.2);
+    //player.setScale(0.9, 0.9);
+    
+    // this.anims.create({
+    // key:'left',
+    // frames: this.anims.generateFrameNumbers('dude', {start:0, end: 3}),
+    // frameRate: 10,
+    // repeat: -1
+    // })
+
+    // this.anims.create({
+    //   key: 'turn',
+    //   frames:[ {key: 'dude', frame: 4}],
+    //   frameRate: 20
+    // })
+
+    // this.anims.create({
+    //   key:'right',
+    //   frames: this.anims.generateFrameNumbers('dude', {start:5, end: 8}),
+    //   frameRate: 10,
+    //   repeat: -1
+    //   })
+    
+    
+    this.physics.add.collider(player, platform);
+    //this.cursors = this.input.keyboard.createCursorKeys()
+    
 
     //this for the diamonds
     diamonds = this.physics.add.group({
       key: 'diamond',
       repeat: 11,
-      setXY:{x: 12, y:0, stepX: 70}
+      setXY:{x: 25, y:0, stepX: 85}
     });
 
     diamonds.children.iterate(function (child){
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
 
-    this.physics.add.collider(diamonds, platforms);
+    //check for collision on platform
+    this.physics.add.collider(diamonds, platform);
+    this.physics.add.overlap(player, diamonds, collectStar, null, this);
+    //diamonds.createMultiple({ key: 'diamonds', frame: [0,1,2,3,4], frameQuantity: 2, repeat: 1 });
+    
+    function collectStar (player, diamonds) {
 
-  
+        diamonds.disableBody(true, true);
+    }
 
-  group = this.add.group();//demo fromphaser page
-  for (let i =0; i <12; i++){
-  let diamond= group.create(i*70, 0, 'diamonds')
-  diamond.createMultiple({ key: 'diamonds', frame: [0,1,2,3,4], frameQuantity: 2, repeat: 1 });
+    player.anims.add('left', [0, 1, 2,], 10,true)
+    player.anims.add('right', [6, 7, 8], 10,true)
   //group.createMultiple({ key: 'diamonds', frame: [0,1,2,3,4], repeat: 4, max: 12 });
-  Phaser.Actions.SetXY(group.getChildren(), 35, 100, 32);
-  }
+  //Phaser.Actions.SetXY(group.getChildren(), 35, 100, 32);
+  //}
     //ground.scale.setTo(2, 2)
     //ground.body.immovable = true
 
     //cloudsWhiteSmall = this.add.tileSprite(640, 200, 1280, 400, "clouds-white-small");
     //icon= this.add.sprite(32, this.height - 150, 'icon');
     //adding the character
-
-    //icon=this.add.sprite(102, 550 , 'icon', 'icon.png');
-    //this is the icon scale
   
+    
 
-
-    
-    
-    
-    
-    
-    for (let i = 0; i <12; i++){
-      //dia.create( i * 70 , 0, 'diamond')
-      dia.body.gravity.y = 1000
-      dia.body.bounce.y = 0.3 + Math.random() * 0.2
-    }
-
-
-    scoreText = game.add.text(16, 16, '', {fontSize: '32px', fill: '#000'})
-    cursors = game.input.keyboard.createCursorKeys()
+    //scoreText = game.add.text(16, 16, '', {fontSize: '32px', fill: '#000'})
+    //cursors = game.input.keyboard.createCursorKeys()
     
  }
+
+
+cursors = this.input.keyboard.createCursorKeys();
+
  function updateScene() {
 //     cloudsWhite.tilePositionX += 0.5;
 //     cloudsWhiteSmall.tilePositionX += 0.25;
+
+if (cursors.left.isDown)
+    {
+        player.body.velocity = -160;
+    
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+      player.body.velocity = 160;
+    
+        player.anims.play('right', true);
+    }
+    else
+    {
+      player.body.velocity = 0;
+    
+        player.anims.play('turn');
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+      player.body.velocity = -330;
+    }
+
+
 }
 
 
@@ -126,25 +178,7 @@ function preloadScene() {
 
 
 
-//function startPlaying() {
-      //phaserConfig.start();
-//     //game piece has the falling parameters
-//     gameItem = new component(30, 30, "blue", 10, 120);
-//}
 
-  
-//     function component(width, height, color, x, y){
-//         this.width = width;
-//         this.height = height;
-//         this.speedX =0;//used to change position
-//         this.speedY =0;//used to change position
-//         this.x = x;
-//         this.y = y;
-//         this.update = function(){
-//         //ctx= gameArea.context;
-//         //ctx.fillStyle =color;
-//         //ctx.fillRect(this.x, this.y, this.width, this.height);
-//     } 
 
     this.newPos = function(){
         this.x += this.speedX;
@@ -178,21 +212,6 @@ function preloadScene() {
       
 
 
-// let gameArea ={
-//     canvas: document.createElement("canvas"),
-//         start : function(){
-//             this.canvas.width = 700;
-//             this.canvas.height = 700;
-//             this.context = this.canvas.getContext("2d")
-
-//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//         this.interval = setInterval(updateGameArea, 20);
-//         },
-//         clear : function() {
-//             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//         }
-
-// }
 
 
 
